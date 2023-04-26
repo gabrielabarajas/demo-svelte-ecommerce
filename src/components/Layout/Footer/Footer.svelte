2a9b116853ff22919ec3029b06d2b758820324d4
Rubén Zambrano <ruben.zambrano@decathlon.com>
2023-04-26 08:21:46 +0200

    feat: add footer

---
+++ src/components/Layout/Footer/Footer.svelte
@@ -0,0 +1,72 @@
+<section
+	class="vtmn-flex vtmn-flex-row vtmn-flex-wrap vtmn-justify-center vtmn-items-center vtmn-bg-border-primary vtmn-w-full vtmn-pt-2"
+>
+	<div class="vtmn-flex vtmn-flex-row vtmn-flex vtmn-gap-6">
+		<div class="vtmn-flex justify-center vtmn-items-center vtmn-flex-col vtmn-my-4">
+			<h3 class="vtmn-typo_title-5">APP DECATHLON</h3>
+			<div class="vtmn-flex vtmn-justify-center vtm-p-2 vtmn-typo_text-2">
+				<a
+					class="vtmn-no-underline vtmn-visited:vtmn-text-black"
+					href="https://apps.apple.com/es/app/decathlon-tienda-de-deporte/id1502069790"
+				>
+					<span class="vtmx-apple-fill vtmn-text-black" />
+					Apple Store
+				</a>
+			</div>
+			<div class="vtmn-justify-center vtm-p-2">
+				<a
+					class="vtmn-no-underline vtmn-visted:vtmn-text-black vtmn-typo_text-2"
+					href="https://play.google.com/store/apps/details?id=com.decathlon.appdecathlon "
+				>
+					<span class="vtmx-android-fill vtmn-text-black" />
+					Google Play
+				</a>
+			</div>
+		</div>
+
+		<div class="vtmn-flex justify-center vtmn-items-center vtmn-flex-col vtmn-my-4">
+			<h3 class="vtmn-mx-2 vtmn-typo_title-5">REDES SOCIALES</h3>
+			<div class="vtmn-flex vtmn-p-2">
+				<div class="vtmn-mx-2">
+					<a class="" href="https://es-es.facebook.com/decathlonespana">
+						<span class="vtmx-facebook-fill vtmn-text-back" />
+					</a>
+				</div>
+				<div class="vtmn-mx-2">
+					<a class="" href="https://www.youtube.com/user/DecathlonEsp">
+						<span class="vtmx-youtube-fill vtmn-text-back" />
+					</a>
+				</div>
+				<div class="vtmn-mx-2">
+					<a class="" href="https://www.instagram.com/decathlones/?hl=en">
+						<span class="vtmx-instagram-fill vtmn-text-back" />
+					</a>
+				</div>
+				<div class=" vtmn-mx-2">
+					<a href="https://twitter.com/decathlones">
+						<span class="vtmx-twitter-fill vtmn-text-back" />
+					</a>
+				</div>
+				<div class="vtmn-mx-2">
+					<a class="" href="https://es.pinterest.com/decathlones/">
+						<span class=" vtmx-pinterest-fill vtmn-text-back" />
+					</a>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div class="vtmn-p-4 vtmn-typo_caption-1 vtmn-text-justify">
+		<p>
+			*Consulta los métodos, plazos y gastos de envío disponibles en el siguiente <a
+				href="https://www.decathlon.es/es/landing/tipos-envio-plazos-entrega/_/R-a-tipos-envio-plazos-entrega"
+				target="_blank"
+				rel="noopener">enlace.</a
+			>
+			**Los precios de venta de los artículos de nuestro catálogo incluyen IVA. Los precios para las
+			entregas con destino a Canarias, Ceuta, Melilla y Andorra incluyen impuestos indirectos. Los precios
+			de venta de los artículos de nuestro catálogo son los recomendados desde central a día de ayer.
+			Gastos de envío no incluidos. Fotos de los productos no contractuales.
+		</p>
+	</div>
+</section>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,6 @@
 <script>
 	import Header from '../components/Layout/Header/Header.svelte';
+	import Footer from '../components/Layout/Footer/Footer.svelte';
 </script>
 
 <svelte:head>
@@ -7,32 +8,36 @@
 	<link rel="stylesheet" href="https://unpkg.com/@vtmn/icons@1.2.1/dist/vitamix/font/vitamix.css" />
 </svelte:head>
 
-<Header />
+<section class="vtmn-flex vtmn-flex-col vtmn-min-h-screen">
+	<Header />
 
-<section>
-	<div class="vtmn-p-4 vtmn-grow">
-		<div class="vtmn-mb-4 vtmn-typo_text-3">
-			<div id="search" class="vtmn-mb-4">
-				<!-- Muestra un mensaje que enseñe la búsqueda actual y avise al usuario de si su búsqueda tiene o no resultados -->
+	<section class="vtmn-flex-grow">
+		<div class="vtmn-p-4 vtmn-grow">
+			<div class="vtmn-mb-4 vtmn-typo_text-3">
+				<div id="search" class="vtmn-mb-4">
+					<!-- Muestra un mensaje que enseñe la búsqueda actual y avise al usuario de si su búsqueda tiene o no resultados -->
+				</div>
+
+				<div class="vtmn-divider vtmn-mb-4" />
+
+				<div id="filters">
+					<!-- Renderiza los filtros y haz que sean reactivos con la opción que elija el usuario -->
+				</div>
 			</div>
 
 			<div class="vtmn-divider vtmn-mb-4" />
 
-			<div id="filters">
-				<!-- Renderiza los filtros y haz que sean reactivos con la opción que elija el usuario -->
+			<div id="products">
+				<div class="vtmn-typo_text-2 vtmn-mb-1 vtmn-underline">LISTADO DE PRODUCTOS</div>
+
+				<div id="products" class="vtmn-grid vtmn-grid-cols-2 gt-tablet:vtmn-grid-cols-4">
+					<!-- Renderiza el listado de productos usando el component <Product /> para pintar cada uno de ellos> -->
+				</div>
 			</div>
 		</div>
+	</section>
 
-		<div class="vtmn-divider vtmn-mb-4" />
-
-		<div id="products">
-			<div class="vtmn-typo_text-2 vtmn-mb-1 vtmn-underline">LISTADO DE PRODUCTOS</div>
-
-			<div id="products" class="vtmn-grid vtmn-grid-cols-2 gt-tablet:vtmn-grid-cols-4">
-				<!-- Renderiza el listado de productos usando el component <Product /> para pintar cada uno de ellos> -->
-			</div>
-		</div>
-	</div>
+	<Footer />
 </section>
 
 <style>
